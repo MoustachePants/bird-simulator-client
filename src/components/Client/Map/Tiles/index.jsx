1872e927d1513f9c484bf4bb44eeacd6b5e6f080
elad <elad222@gmail.com>
2024-08-31 19:14:03 +0300

    netlify adjustments

--- src/components/Client/Map/Tiles/index.jsx
+++ src/components/Client/Map/Tiles/index.jsx
@@ -5,14 +5,14 @@ const Tiles = ({ selectedTile }) => {
     <>
       {selectedTile === "Water Color" && (
         <TileLayer // ! if I upload online I should add the attribution!
-          // attribution='Index tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Index data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='Index tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Index data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}"
           ext="jpg"
         />
       )}
       {selectedTile === "Satellite" && (
         <TileLayer // ! if I upload online I should add the attribution!
-          // attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
           url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
         />
       )}
