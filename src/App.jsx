9fcad3909f4cb9a8c2e837b7f4bbe2b59600cf1b
elad <elad222@gmail.com>
2024-08-31 22:27:40 +0300

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Client from "./components/Client";
 import DC from "./components/DC";
-
+import Loader from "./components/UI/Loader";
 import { useEffect, useState } from "react";
 import { APIURL, intervalRate, runFrontInterval } from "../config.js";
 import birdFinder from "./utils/birdFinder.js";
 
 const App = () => {
   const [birdsData, setBirdsData] = useState([]);
+  const [isLoadingData, setIsLoadingData] = useState(true);
   const [selectedBirdIndex, setSelectedBirdIndex] = useState(null);
   const [selectedEyeViewBirdIndex, setSelectedEyeViewBirdIndex] =
     useState(null);
@@ -33,6 +34,7 @@ const App = () => {
       const fetchBirdsData = async () => {
         const res = await fetch(APIURL);
         const data = await res.json();
+        setIsLoadingData(false);
         setBirdsData(data);
       };
 
@@ -42,6 +44,12 @@ const App = () => {
 
   return (
     <div className="App">
+      {isLoadingData && (
+        <div className="loading-screen">
+          <Loader />
+          <p>Loading might take few seconds...</p>
+        </div>
+      )}
       <Client
         birdsData={birdsData}
         selectedBirdTailNum={birdsData[selectedBirdIndex]?.tailNum}
